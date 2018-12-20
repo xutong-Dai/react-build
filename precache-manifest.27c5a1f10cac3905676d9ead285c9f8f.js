789d440b2da8482da7e9cf613d1906fa640858a8
xutong-Dai <945016828@qq.com>
2018-12-20 15:00:37 +0800

    Updates

---
+++ precache-manifest.27c5a1f10cac3905676d9ead285c9f8f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "cad3f1956c85412e7e38",
+    "url": "/react-build/static/js/main.cad3f195.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/react-build/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "346190b56305f5cddf8c",
+    "url": "/react-build/static/js/runtime~main.346190b5.js"
+  },
+  {
+    "revision": "6431be84d64be6b060b814a6cb126050",
+    "url": "/react-build/index.html"
+  }
+];
